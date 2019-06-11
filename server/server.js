3a1bcc83b9033322f807f20b02463b639df01336
NickRashad <nrsternes@gmail.com>
2019-06-10 23:29:11 -0500

    Successfully start proxy server w/ Reviews & User Service

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.static('public'));
 
-app.use('/biz', proxy({ target: 'localhost/3001', changeOrigin: true }));
-app.use('/biz_photos', proxy({ target: 'localhost/3001', changeOrigin: true }));
-app.use('/user', proxy({ target: 'localhost/3002', changeOrigin: true }));
-app.use('/users', proxy({ target: 'localhost/3002', changeOrigin: true }));
-app.use('/reviews', proxy({ target: 'localhost/3002', changeOrigin: true }));
+app.use('/biz', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/biz_photos', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/user', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/users', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/reviews', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
