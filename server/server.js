52b59daf043e182a3a7577ece4853043914f18ee
NickRashad <nrsternes@gmail.com>
2019-06-26 22:34:56 -0500

    Create env variables for proxy servers

--- server/server.js
+++ server/server.js
@@ -3,16 +3,17 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.static('public'));
 app.use('/bizs/:bId', express.static(`${__dirname}/../public`));
 
-app.use('/biz', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/biz_photos', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/user', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use('/users', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use('/reviews', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/biz', proxy({ target: process.env.BIZSERVICE, changeOrigin: true }));
+app.use('/biz_photos', proxy({ target: process.env.BIZSERVICE, changeOrigin: true }));
+app.use('/user', proxy({ target: process.env.REVIEWSERVICE, changeOrigin: true }));
+app.use('/users', proxy({ target: process.env.REVIEWSERVICE, changeOrigin: true }));
+app.use('/reviews', proxy({ target: process.env.REVIEWSERVICE, changeOrigin: true }));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
